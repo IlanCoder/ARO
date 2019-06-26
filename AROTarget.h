0f76a7f8581df3cfd9185c98b79cc25b2261de2e
Ilan Gonzalez Carigi <ilancoder00@gmail.com>
2019-06-26 19:59:07 +0900

    A.R.O. Project
    The 2 A.R.O. scripts and a README.

---
+++ AROTarget.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AROTarget.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NOERROR_API UAROTarget : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAROTarget();
+
+protected:
+	virtual void BeginPlay() override;
+	TArray<FVector> externalForces;
+	AActor* myBoss;
+	void ResetExternalForces();
+	void CalculateForces(float DeltaTime);
+	UPROPERTY(EditAnywhere)
+	FVector velocity;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void SetExternalForce(FVector force);
+	void SetVelocity(FVector newVelocity);
+	FVector GetVelocity();
+};
---
+++ README.txt.txt
@@ -0,0 +1,16 @@
+Thank you for downloading A.R.O.
+Made by: Ilan Gonzalez Carigi
+This project is open source and can be used in any project as long as the proper credits are mentioned
+
+A.R.O. is a set of 2 scripts to help developers in integrating gravitacional physicics in their games 
+without the need to use heavy formulas for simululations and also enabling customization in the formula
+for more game-oriented possibilities and results
+
+The code is written in c++ for the UE4, but can be easily changed to other languages
+due to it's null dependency to the engine.
+
+Warning:
+The code will give not act as expected in the case of:
+A) Max Distance is set to an incredibe high number (will be heavy on the machine).
+B) If the AROTarget positions is the same as the ARO in any given time.
+
